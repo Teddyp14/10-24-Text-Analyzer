43dcdd5b5a45175dc6109661cda509a001ac2d74
Teddy Peterschmidt <t.a.peterschmidt@gmail.com>
2023-10-24 17:04:36 -0700

    Add a offensive word omitting function to the js and pseudocode tests to the README.
    Co-authored-by: Grant Abel <gtabel88@gmail.com>

--- README.md
+++ README.md
@@ -100,3 +100,45 @@ Code:
 const word4 = loopdaloop
 omitOffensiveWords("There they go 'round the loopdaloop.")
 Expected output: "There they go 'round the Kelly Clarkson."
+
+Describe: boldPassage()
+
+Test: "It should return null if no word or text is entered."
+Code:
+const text = "";
+const word = "";
+boldPassage(word, text);
+Expected Output: null
+
+Test: "It should return a non-matching word in a p tag."
+Code:
+const word = "hello";
+const text = "yo";
+boldPassage(word, text);
+Expected Output: <p>yo</p>
+
+Test: "It should return a matching word in a strong tag."
+Code:
+const word = "hello";
+const text = "hello";
+boldPassage(word, text);
+Expected Output: <p><strong>hello</strong></p>
+
+Test: "It should wrap words that match in strong tags but not words that don't."
+Code:
+const word = "hello";
+const text = "hello there";
+boldPassage(word, text);
+Expected Output: <p><strong>hello</strong>there</p>
+
+Describe: numberOfTimesWordAppears()
+
+Test: "It should return a list of each word used and a count of the number of times it is used."
+Code:
+const string = "Hello."
+Expected output: "Hello: 1"
+
+Test: "It should return the list of words used in an alphabetical order, with count of occurence."
+Code:
+const string = "Hello Jerry, isn't it a swell day?."
+Expected output: "a: 1; day: 1; hello: 1; isn't: 1; it: 1; jerry: 1; swell: 1"
--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,14 @@
+//Utility Logic
+
+function isEmpty(testString) {
+    return (testString.trim().length === 0);
+}
+
 //business logic 
 
 function wordCounter(text) {
-    if (text.trim().length === 0) {
-        return 0;
+    if (isEmpty(text)) {
+        return 0
     }
     let wordCount = 0;
     const textArray = text.split(" ");
@@ -14,7 +20,10 @@ function wordCounter(text) {
     return wordCount;
 }
 
-function numberOfOCcurenencesInText(word, text) {
+function numberOfOccurenencesInText(word, text) {
+    if (isEmpty(word)) {
+        return 0;
+    }
     const textArray = text.split(" ");
     let wordCount = 0;
     textArray.forEach(function (element) {
@@ -25,22 +34,70 @@ function numberOfOCcurenencesInText(word, text) {
     return wordCount;
 }
 
-function omitsOffensiveWords(text) {
-    const censoredString = text.toLowerCase().replace("zoinks", "Kelly Clarkson");
-    const censoredTwice = censoredString.toLowerCase().replace("muppeteer", "Kelly Clarkson");
-    const censoredThrice = censoredTwice.toLowerCase().replace("biffaroni", "Kelly Clarkson");
-    const censoredFourTimes = censoredThrice.toLowerCase().replace("loopdaloop", "Kelly Clarkson")
-    return censoredFourTimes;
-}
-
 function omitsOffensiveWordsV2(text) {
     const textArray = text.split(" ");
     let = censoredArray = [];
     textArray.forEach(function (element) {
-        if (element.toLowerCase() !== "zoinks" && element.toLowerCase() !== "muppeteer" && element.toLowerCase() !== "biffaroni" && element.toLowerCase() !== "loopdaloop") {
+        if (element.toLowerCase() !== "zoinks" &&
+            element.toLowerCase() !== "muppeteer" &&
+            element.toLowerCase() !== "biffaroni" &&
+            element.toLowerCase() !== "loopdaloop") {
             censoredArray.push(element);
         }
     })
 
     return censoredArray.join(" ");
 }
+
+function numberOfTimesWordAppears(text) {
+    const textArray = text.toLowerCase().split(" ");
+    let wordCounterArray = []
+    textArray.forEach(function (element) {
+        wordCounterArray.push({ element, count: 1 })
+    })
+    return wordCounterArray
+}
+
+
+//UI Logic
+function boldPassage(word, text) {
+    if (isEmpty(word) || isEmpty(text)) {
+        return null;
+    }
+    const p = document.createElement("p");
+    let textArray = text.split(" ");
+    textArray.forEach(function (element, index) {
+        if (word === element) {
+            const bold = document.createElement("strong");
+            bold.append(element);
+            p.append(bold);
+        } else {
+            p.append(element);
+        }
+        if (index !== (textArray.length - 1)) {
+            p.append(" ");
+        }
+    });
+    return p;
+}
+
+function handleFormSubmission() {
+    event.preventDefault();
+    const passage = document.getElementById("text-passage").value;
+    const word = document.getElementById("word").value;
+    const wordCount = wordCounter(passage);
+    const occurrencesOfWord = numberOfOccurenencesInText(word, passage);
+    document.getElementById("total-count").innerText = wordCount;
+    document.getElementById("selected-count").innerText = occurrencesOfWord;
+
+    let boldedPassage = boldPassage(word, passage);
+    if (boldedPassage) {
+        document.querySelector("div#bolded-passage").append(boldedPassage);
+    } else {
+        document.querySelector("div#bolded-passage").innerText = null;
+    }
+}
+
+window.addEventListener("load", function () {
+    document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
+});
